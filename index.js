d794aa91ee00ee9184c067cb9d7949ed72cce215
brianhart <brianhart@fuller.edu>
2020-11-05 10:33:31 -0800

    added folder to url

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 if('serviceWorker' in navigator){
     window.addEventListener('load', () => {
         navigator.serviceWorker
-			.register('/sw_cached_site.js')
+			.register('/tic-tac-toe-vanilla/sw_cached_site.js/')
             .then(console.log('Servicewroker registered'))
             .catch(err => console.log(`ServiceWorker error: ${err}`));
     });
