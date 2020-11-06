70442cc9a5becae11afd72fec661b3effe1433e5
Brian-T-Hart <hart.briant@gmail.com>
2020-11-05 21:56:02 -0800

    add conditional for sw local

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
+// register service worker
+var registerUrl = '/sw_cached_site.js';
+var swScope = '/';
+
+if (location.hostname === 'brian-t-hart.github.io') {
+	registerUrl = '/tic-tac-toe-vanilla/sw_cached_site.js';
+	swScope = '/tic-tac-toe-vanilla/';
+}
+
 //make sure sw are supported
 if('serviceWorker' in navigator){
     window.addEventListener('load', () => {
         navigator.serviceWorker
-			.register('/tic-tac-toe-vanilla/sw_cached_site.js', {scope: '/tic-tac-toe-vanilla/'})
+			.register(registerUrl)
             .then(console.log('Servicewroker registered'))
             .catch(err => console.log(`ServiceWorker error: ${err}`));
     });
