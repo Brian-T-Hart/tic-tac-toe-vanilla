927ca86e4db07e9c8e93d079831b629652e9d6e3
Brian-T-Hart <hart.briant@gmail.com>
2022-09-05 13:40:15 -0700

    moved serviceworker reg to separate file

--- index.js
+++ index.js
@@ -1,26 +1,3 @@
-// register service worker
-var registerUrl = '/tic-tac-toe-vanilla/sw_cached_site.js';
-var swScope = '/tic-tac-toe-vanilla/';
-
-if (location.hostname !== 'brian-t-hart.github.io') {
-	registerUrl = '/sw_cached_site.js';
-	swScope = '/';
-}
-
-//make sure sw are supported
-if('serviceWorker' in navigator){
-    window.addEventListener('load', () => {
-        navigator.serviceWorker
-			.register(registerUrl, {scope: swScope})
-            .then(console.log('Servicewroker registered'))
-            .catch(err => console.log(`ServiceWorker error: ${err}`));
-    });
-}//if SW
-
-else {
-    console.log('serviceWorker not in navigator');
-}
-
 // tic-tac-toe logic
 
 var boxes = document.getElementsByClassName('box');
---
+++ sw_register.ts
@@ -0,0 +1,21 @@
+// register service worker
+var registerUrl: string = '/sw_cached_site.js';
+var swScope: string = '/';
+
+if (location.hostname == 'brian-t-hart.github.io') {
+    registerUrl = '/tic-tac-toe-vanilla/sw_cached_site.js';
+    swScope = '/tic-tac-toe-vanilla/';
+}
+
+//make sure sw are supported
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker
+            .register(registerUrl, { scope: swScope })
+            .then(() => console.log('Serviceworker registered'))
+            .catch(err => console.log(`ServiceWorker error: ${err}`));
+    });
+
+} else {
+    console.log('serviceWorker not in navigator');
+}
